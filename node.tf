0f56374e67e9fe87312f5e601b08ac31c31f263a
dsolovew <dsolovew@gmail.com>
2020-12-24 20:03:02 +0300

    group

--- node.tf
+++ node.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "us-east-1"
  }
 
- resource "aws_security_group" "myGroup1" {
-   name = "group1"
+ resource "aws_security_group" "myGroup2" {
+   name = "group2"
    description = "security group"
      
    ingress  {
@@ -27,11 +27,11 @@ provider "aws" {
    ami = "ami-00ddb0e5626798373"
    instance_type = "t3.micro"
    vpc_security_group_ids  = [aws_security_group.myGroup1.id]
-   key_name = "ssh-key"
+   key_name = "ssh-key2"
    count = 2
  }
  
-resource "aws_key_pair" "ssh-key" {
-  key_name = "ssh-key"
+resource "aws_key_pair" "ssh-key2" {
+  key_name = "ssh-key2"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCd1asempe53PptCHEZJ1Hp+biFNF4PHNkW2N7wIlCMJXAsLRQo6TsuMGe+W6B2t+dtMrj4czq5OBEUb/aS4rwVa/EtWeoBC7749xiZIyt0wUsLFEardw07hoazj96mFRZmpkJ3rd8CKU+grXMiSNwBQcVKR1rg3MVt/RwQdzC8t/ufNKqKwzkNvW/lSkl6WZjQU/ZdI9TKsapK5Xqjv9IBBXnfo5b7SVt56F+LOPNNSBKrJyR+LRyiADESfOITsqldeveeUIGC40Vwh48ZBeBzBbAwCX7wj5T80ThoNGTNdtWH/uQA1XI2TPs2e7GVv4Krn0aLfk4h3r+ol6+KN8aX"
 }
